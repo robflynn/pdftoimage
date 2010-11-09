4c41d85926b50d2b7394f6cc865f1ccaee1c4294
Rob Flynn <rob@thingerly.com>
2010-11-09 07:56:49 +0000

    Added a couple more tests.

--- spec/pdftoimage_spec.rb
+++ spec/pdftoimage_spec.rb
@@ -11,5 +11,21 @@ describe PDFToImage do
     @pages = PDFToImage.open('spec/3pages.pdf')
       @pages.size.should equal 3
     end
+
+    it "should allow saving" do
+      @pages = PDFToImage.open('spec/3pages.pdf')
+      @pages[0].save('spec/tmp.jpg')
+      File.exists?('spec/tmp.jpg').should equal true
+      File.unlink('spec/tmp.jpg')
+    end
+
+    it "should allow resizing" do
+      @pages = PDFToImage.open('spec/3pages.pdf')
+      @pages[0].resize('50%').save('spec/tmp2.jpg')
+      File.exists?('spec/tmp2.jpg').should equal true
+      File.unlink('spec/tmp2.jpg')
+    end
+
+
   end
 end
